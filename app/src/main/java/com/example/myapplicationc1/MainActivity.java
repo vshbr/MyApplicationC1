b710f7765917b94071a8e6124e0dc1cd4b93227f
Jolyadd <dredd0077@gmial.com>
2024-06-17 17:37:43 +0300

    updt

--- app/src/main/java/com/example/myapplicationc1/MainActivity.java
+++ app/src/main/java/com/example/myapplicationc1/MainActivity.java
@@ -2,28 +2,35 @@ package com.example.myapplicationc1;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private int currentScore = 0;
+    int number_of_clicks  = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imgCookie = findViewById(R.id.imgCookie);
+    }
 
-        imgCookie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                currentScore ++;
-                Toast.makeText(MainActivity.this, "your score is " + currentScore, Toast.LENGTH_SHORT).show();
-            }
-        });
+
+    public void onClick1(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        number_of_clicks++;
+        textView.setText("Печенек: "+number_of_clicks);
+    }
+
+
+    public void onClick(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        number_of_clicks--;
+        textView.setText("Печенек: "+number_of_clicks);
     }
 
 }
