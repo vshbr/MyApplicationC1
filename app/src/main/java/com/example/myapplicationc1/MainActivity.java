4005a141e2424824067d4909e93ed68d48639700
vshbr <ivanshabor04@gmail.com>
2024-06-22 15:53:24 +0300

    upd 3

--- app/src/main/java/com/example/myapplicationc1/MainActivity.java
+++ app/src/main/java/com/example/myapplicationc1/MainActivity.java
@@ -11,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     int number_of_clicks  = 0;
+    int fuve = 5; //числа которые что-то делаю;
+    int number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,32 @@ public class MainActivity extends AppCompatActivity {
     public void onClick1(View view) {
         TextView textView = (TextView) findViewById(R.id.textView);
         number_of_clicks++;
-        textView.setText("Печенек: "+number_of_clicks);
+        textView.setText("Печенек: " + number_of_clicks);
     }
 
 
     public void onClick(View view) {
         TextView textView = (TextView) findViewById(R.id.textView);
         number_of_clicks--;
-        textView.setText("Печенек: "+number_of_clicks);
+        textView.setText("Печенек: " + number_of_clicks);
+    }
+
+    public void onClick2(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        number_of_clicks = 0;
+        textView.setText("Печенек: " + number_of_clicks);
+    }
+
+    public void onClick6(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        number_of_clicks = number_of_clicks + 5 ;
+        textView.setText("Печенек: " + number_of_clicks);
+    }
+
+    public void onClick5(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        number_of_clicks = number_of_clicks - 5;
+        textView.setText("Печенек: " + number_of_clicks);
     }
 
 }
